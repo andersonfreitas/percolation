ac7b2c54a406d68354890a9874694c9ac0875ce5
Anderson Freitas <anderson.amf@gmail.com>
2012-09-22 01:38:26 -0300

    Removing app/ (to be compiled from coffee)

--- app/modules/editor.js
+++
@@ -1,9 +0,0 @@
-define(["app"], function(app) {
-  var Editor;
-  Editor = app.module();
-  Editor.Site = Backbone.Model.extend({});
-  Editor.Collection = Backbone.Collection.extend({
-    model: Editor.Site
-  });
-  return Editor;
-});
